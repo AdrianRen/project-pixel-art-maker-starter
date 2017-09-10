74ccd2127daf08d33681beea5513e7b135ed2321
Adrian Ren <adrian.ren@outlook.com>
2017-09-10 22:15:28 +1200

    Final check

--- designs.js
+++ designs.js
@@ -1,12 +1,11 @@
 const $sizePicker = $('#sizePicker'),
-// Define global variables
-  $input_height = $('#input_height'),
-  $input_width = $('#input_width'),
-  $colorPicker = $('#colorPicker'),
-  $pixel_canvas = $('#pixel_canvas');
+      $input_height = $('#input_height'),
+      $input_width = $('#input_width'),
+      $colorPicker = $('#colorPicker'),
+      $pixel_canvas = $('#pixel_canvas');
 let color = 'black';
 
-// When size is submitted by the user, call makeGrid()
+
 let makeGrid = (row, column) => {
   let html;
   for (let x = 0; x < row; x++) {
@@ -19,6 +18,7 @@ let makeGrid = (row, column) => {
   $pixel_canvas.append(html);
 }
 
+
 let deleteRow = () => {
   let rowCounter = $('tr').length;
   while (rowCounter--) {
@@ -30,15 +30,15 @@ $colorPicker.change(() => {
   color = $colorPicker.val();
 });
 
+
 $sizePicker.submit(function(e) {
   e.preventDefault();
   let row = $input_height.val(),
-      column = $input_width.val(),
-      trs = $('tr').length;
-  // $('#pixel_canvas tr').remove();
+    column = $input_width.val(),
+    trs = $('tr').length;
   deleteRow();
   makeGrid(row, column);
-  $('td').click(function(){
-    $(this).css('background-color',color);
+  $('td').click(function() {
+    $(this).css('background-color', color);
   })
 });
