56eea5ed2337566038697a04bf0ad94edecd123d
Adrian Ren <adrian.ren@outlook.com>
2017-09-10 22:13:28 +1200

    updates

--- designs.js
+++ designs.js
@@ -4,7 +4,8 @@ const $sizePicker = $('#sizePicker'),
   $input_width = $('#input_width'),
   $colorPicker = $('#colorPicker'),
   $pixel_canvas = $('#pixel_canvas');
-let color;
+let color = 'black';
+
 // When size is submitted by the user, call makeGrid()
 let makeGrid = (row, column) => {
   let html;
@@ -18,6 +19,13 @@ let makeGrid = (row, column) => {
   $pixel_canvas.append(html);
 }
 
+let deleteRow = () => {
+  let rowCounter = $('tr').length;
+  while (rowCounter--) {
+    document.getElementById('pixel_canvas').deleteRow(-1);
+  }
+}
+
 $colorPicker.change(() => {
   color = $colorPicker.val();
 });
@@ -25,8 +33,10 @@ $colorPicker.change(() => {
 $sizePicker.submit(function(e) {
   e.preventDefault();
   let row = $input_height.val(),
-      column = $input_width.val();
-  $('#pixel_canvas tr').remove();
+      column = $input_width.val(),
+      trs = $('tr').length;
+  // $('#pixel_canvas tr').remove();
+  deleteRow();
   makeGrid(row, column);
   $('td').click(function(){
     $(this).css('background-color',color);
