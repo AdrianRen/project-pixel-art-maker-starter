d6c54d542dba695af15dc447ca7d5425f0617b8d
Adrian Ren <adrian.ren@outlook.com>
2017-09-10 14:09:49 +1200

    updates

--- designs.js
+++ designs.js
@@ -1,19 +1,35 @@
 
 // Define global variables
-let $sizePicker = $('#sizePicker'),
-    $input_height = $('#input_height'),
-    $input_width = $('#input_width'),
-    $colorPicker = $('#colorPicker'),
-    $pixel_canvas = $('#pixel_canvas');
+const $sizePicker = $('#sizePicker'),
+      $input_height = $('#input_height'),
+      $input_width = $('#input_width'),
+      $colorPicker = $('#colorPicker'),
+      $pixel_canvas = $('#pixel_canvas');
 
-$sizePicker.submit(function () {
-  let x = $input_width.val(),
-      y = $input_height.val();
-  console.log(x,y);
-  return false;
-})
 // When size is submitted by the user, call makeGrid()
-let makeGrid = (x,y) => {
-  
-
+let makeGrid = (row,column) => {
+  let html;
+  for (let x = 0; x < row; x++) {
+    html += `<tr>`;
+    for (var y = 0; y < column; y++) {
+      html += `<td class="pixel"></td>`
+    }
+    html += `</tr>`;
+  }
+  $pixel_canvas.append(html);
 }
+
+let clearGrid = () => {
+  let $tr = $('tr').length;
+  while ($tr > 0) {
+    $pixel_canvas.deleteRow(0);
+  }
+}
+
+$sizePicker.submit(function (e) {
+  e.preventDefault();
+  let row = $input_height.val(),
+      column = $input_width.val();
+  clearGrid();
+  makeGrid(row,column);
+});
